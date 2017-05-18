93922d382120bf4eb1d6cd5a0439a9ad3bf07ed0
Luke Farrawell <luke.farrawell@uon.edu.au>
2017-05-18 13:51:02 +1000

    Added addTrace to the plotly type definitions

--- declarations/plotly.js/index.d.ts
+++ declarations/plotly.js/index.d.ts
@@ -3,8 +3,11 @@ import 'plotly.js'
 declare module 'plotly.js' {
   type Update = { x: Array<number[]>, y: Array<number[]> }
   type ShortLayout = { width: number, height: number }
+  type ShortMarker = {size: number}
+  type ShortData = {x: Array<number[]>, y: Array<number[]>, z: Array<number[]>, mode: string, type: string, marker: ShortMarker, name: string}
   interface PlotlyStatic {
     extendTraces(element: HTMLDivElement, update: Update, indices: number[], maxPoints?: number): void
     relayout(element: HTMLDivElement, layout: ShortLayout): void
+    addTraces(element: HTMLDivElement, traces: ShortData, newIndices?: number[]): void
   }
 }

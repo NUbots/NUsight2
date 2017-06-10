10a666b892ec7d1682b16497601a916640b3bba9
Luke Farrawell <luke.farrawell@uon.edu.au>
2017-06-10 15:04:37 +1000

    Changed Array<number[]> to number[][]

--- declarations/plotly.js/index.d.ts
+++ declarations/plotly.js/index.d.ts
@@ -1,10 +1,10 @@
 // Refer to: http://stackoverflow.com/a/39989713
 import 'plotly.js'
 declare module 'plotly.js' {
-  type Update = { x: Array<number[]>, y: Array<number[]> }
+  type Update = { x: number[][], y: number[][] }
   type ShortLayout = { width: number, height: number }
   type ShortMarker = {size: number}
-  type ShortData = {x: Array<number[]>, y: Array<number[]>, z: Array<number[]>, mode: string, type: string, marker: ShortMarker, name: string}
+  type ShortData = {x: number[][], y: number[][], z: number[][], mode: string, type: string, marker: ShortMarker, name: string}
   interface PlotlyStatic {
     extendTraces(element: HTMLDivElement, update: Update, indices: number[], maxPoints?: number): void
     relayout(element: HTMLDivElement, layout: ShortLayout): void

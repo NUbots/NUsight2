62ec5d12ded05214495d937dda3a1f20a00ae1d6
Brendan Annable <brendan.a@canva.com>
2017-06-17 11:58:19 +1000

    Add sensor data simulator

---
+++ src/simulators/sensor_data_simulator.ts
@@ -0,0 +1,63 @@
+import { message } from '../../src/shared/proto/messages'
+import { RobotSimulator } from './robot_simulator'
+import { SimulatorStatus } from './robot_simulator'
+import { Simulator } from './simulator'
+import { Message } from './simulator'
+import Sensors = message.input.Sensors
+
+export class SensorDataSimulator implements Simulator {
+  public static of() {
+    return new SensorDataSimulator()
+  }
+
+  public simulate(time: number): Message[] {
+    const messageType = 'message.input.Sensors'
+
+    // Simulate a walk
+    const t = time * 5E-3
+
+    const buffer = Sensors.encode({
+      servo: [
+        { presentPosition: 3 * Math.PI / 4 + 0.5 * Math.cos(t - Math.PI) },
+        { presentPosition: 3 * Math.PI / 4 + 0.5 * Math.cos(t) },
+        { presentPosition: Math.PI / 8 },
+        { presentPosition: Math.PI / 8 },
+        { presentPosition: -3 * Math.PI / 4 },
+        { presentPosition: -3 * Math.PI / 4 },
+        { presentPosition: 0 },
+        { presentPosition: 0 },
+        { presentPosition: 0 },
+        { presentPosition: 0 },
+        { presentPosition: 0.5 * (Math.cos(t) - 1) },
+        { presentPosition: 0.5 * (Math.cos(t - Math.PI) - 1) },
+        { presentPosition: 0.5 * (-Math.cos(t) + 1) },
+        { presentPosition: 0.5 * (-Math.cos(t - Math.PI) + 1) },
+        { presentPosition: 0 },
+        { presentPosition: 0 },
+        { presentPosition: 0 },
+        { presentPosition: 0 },
+        { presentPosition: 0.1 * Math.cos(t) },
+        { presentPosition: 0.1 * Math.cos(t / 3) + 0.4 },
+      ],
+    }).finish()
+
+    const message = { messageType, buffer }
+
+    return [ message ]
+  }
+}
+
+function main() {
+  const robotSimulator = RobotSimulator.of({
+    name: 'Sensors Simulator',
+    simulators: [
+      SensorDataSimulator.of(),
+    ],
+  })
+  SimulatorStatus.of(robotSimulator).statusEvery(2)
+  robotSimulator.simulateWithFrequency(60)
+}
+
+if (require.main === module) {
+  main()
+}

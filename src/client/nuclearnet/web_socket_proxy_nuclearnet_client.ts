42ea0e287386d3484c6e34043a9b74ebbc2f6502
Brendan Annable <brendan.a@canva.com>
2017-07-10 22:52:10 +1000

    Add state resync on reconnect

--- src/client/nuclearnet/web_socket_proxy_nuclearnet_client.ts
+++ src/client/nuclearnet/web_socket_proxy_nuclearnet_client.ts
@@ -8,9 +8,15 @@ import SocketIOSocket = SocketIOClient.Socket
 
 export class WebSocketProxyNUClearNetClient implements NUClearNetClient {
   private nextMessageId: number
+  private joinListeners: Set<NUClearEventListener>
+  private leaveListeners: Set<NUClearEventListener>
+  private packetListeners: Map<string, Set<{ messageId: string, listener: NUClearPacketListener }>>
 
   public constructor(private socket: WebSocketClient) {
     this.nextMessageId = 0
+    this.joinListeners = new Set()
+    this.leaveListeners = new Set()
+    this.packetListeners = new Map()
   }
 
   public static of() {
@@ -32,23 +38,45 @@ export class WebSocketProxyNUClearNetClient implements NUClearNetClient {
     }
   }
 
-  public onJoin(cb: NUClearEventListener): () => void {
-    this.socket.on('nuclear_join', cb)
-    return () => this.socket.off('nuclear_join', cb)
+  public onJoin(listener: NUClearEventListener): () => void {
+    this.socket.on('nuclear_join', listener)
+    this.joinListeners.add(listener)
+    return () => {
+      this.socket.off('nuclear_join', listener)
+      this.joinListeners.delete(listener)
+    }
   }
 
-  public onLeave(cb: NUClearEventListener): () => void {
-    this.socket.on('nuclear_leave', cb)
-    return () => this.socket.off('nuclear_leave', cb)
+  public onLeave(listener: NUClearEventListener): () => void {
+    this.socket.on('nuclear_leave', listener)
+    this.leaveListeners.add(listener)
+    return () => {
+      this.socket.off('nuclear_leave', listener)
+      this.leaveListeners.delete(listener)
+    }
   }
 
-  public on(event: string, cb: NUClearPacketListener): () => void {
+  public on(event: string, listener: NUClearPacketListener): () => void {
     const messageId = String(this.nextMessageId++)
     this.socket.send('listen', event, messageId)
-    this.socket.on(event, cb)
+    this.socket.on(event, listener)
+
+    let packetListeners = this.packetListeners.get(event)
+    if (!packetListeners) {
+      packetListeners = new Set()
+      this.packetListeners.set(event, packetListeners)
+    }
+    const packetListener = { messageId, listener }
+    packetListeners.add(packetListener)
+
     return () => {
       this.socket.send('unlisten', messageId)
-      this.socket.off(event, cb)
+      this.socket.off(event, listener)
+
+      const packetListeners = this.packetListeners.get(event)
+      if (packetListeners) {
+        packetListeners.delete(packetListener)
+      }
     }
   }
 
@@ -60,6 +88,21 @@ export class WebSocketProxyNUClearNetClient implements NUClearNetClient {
 
   private onReconnect = (options: NUClearNetOptions) => {
     this.socket.send('nuclear_connect', options)
-    // TODO (Annable): Also re-listen to everything D:
+
+    // Restore all our listeners
+    for (const joinListener of this.joinListeners) {
+      this.socket.on('nuclear_join', joinListener)
+    }
+
+    for (const leaveListener of this.leaveListeners) {
+      this.socket.on('nuclear_leave', leaveListener)
+    }
+
+    for (const [event, packetListeners] of this.packetListeners.entries()) {
+      for (const packetListener of packetListeners) {
+        this.socket.send('listen', event, packetListener.messageId)
+        this.socket.on(event, packetListener.listener)
+      }
+    }
   }
 }

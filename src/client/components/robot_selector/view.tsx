d3a82c9db7ec2fead6f4d66182b4565f9490dd29
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2017-07-13 00:03:24 +1000

    Add empty state

--- src/client/components/robot_selector/view.tsx
+++ src/client/components/robot_selector/view.tsx
@@ -3,7 +3,8 @@ import * as React from 'react'
 import { dropdownContainer } from '../dropdown_container/view'
 import { Switch } from '../switch/view'
 import { RobotModel } from '../robot/model'
-import Robot from './robot.svg'
+import PlugIcon from './plug.svg'
+import RobotIcon from './robot.svg'
 import * as style from './style.css'
 
 export type RobotSelectorProps = {
@@ -15,7 +16,7 @@ export const RobotSelector = observer((props: RobotSelectorProps) => {
   const { robots, selectRobot } = props
   const dropdownToggle = (
     <button className={style.button}>
-      <Robot/>
+      <RobotIcon />
       Select robots
     </button>
   )
@@ -25,6 +26,19 @@ export const RobotSelector = observer((props: RobotSelectorProps) => {
     <div className={style.robotSelector}>
       <EnhancedDropdown dropdownToggle={dropdownToggle} dropdownMenuStyle={dropdownMenuStyle}>
         <div className={style.robots}>
+          {robots.length === 0 &&
+            <div className={style.empty}>
+              <div className={style.emptyIcon}>
+                <PlugIcon />
+              </div>
+              <div className={style.emptyTitle}>
+                No connected robots
+              </div>
+              <span className={style.emptyDescription}>
+                Run yarn start:sim to simulate robots
+              </span>
+            </div>
+          }
           {robots.map(robot => {
             return (
               <label key={robot.name} className={style.robot}>

7b4b3578842954c7688426e2514dc9befa28a3b3
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2017-07-16 20:42:03 +1000

    Add field width and length to field model

--- src/client/components/dashboard/field/model.ts
+++ src/client/components/dashboard/field/model.ts
@@ -1,16 +1,31 @@
+import { computed } from 'mobx'
 import { observable } from 'mobx'
 import { GroundModel } from '../ground/model'
 
+export type FieldModelOpts = {
+  ground: GroundModel
+}
+
 export class FieldModel {
   @observable public ground: GroundModel
-  
-  constructor(opts: FieldModel) {
+
+  constructor(opts: FieldModelOpts) {
     Object.assign(this, opts)
   }
-  
+
   public static of(): FieldModel {
     return new FieldModel({
       ground: GroundModel.of()
     })
+  }
+
+  @computed
+  public get fieldLength() {
+    return this.ground.dimensions.fieldLength + (this.ground.dimensions.goalDepth * 2)
+  }
+
+  @computed
+  public get fieldWidth() {
+    return this.ground.dimensions.fieldWidth
   }
 }

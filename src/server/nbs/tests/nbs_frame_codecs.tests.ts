49e020574233f4bac0fd7671fbfa9f25198211b8
Brendan Annable <brendan.a@canva.com>
2017-07-18 23:53:59 +1000

    .

--- src/server/nbs/tests/nbs_frame_codecs.tests.ts
+++ src/server/nbs/tests/nbs_frame_codecs.tests.ts
@@ -8,22 +8,36 @@ describe('NbsFrameCodecs', () => {
       const hash = hashType('message.input.sensors')
       const timestamp = 1500379664696000
       const payload = new Buffer(8).fill(0x12)
-      const buffer = encodeFrame({
-        timestamp,
-        hash,
-        payload,
-      })
+      const buffer = encodeFrame({ timestamp, hash, payload })
       expect(buffer.toString('hex')).toEqual('e298a218000000c042f15c9654050010abef8b5398f0d41212121212121212')
     })
-    })
+  })
 
   describe('decoding', () => {
     it('decodes frames', () => {
       const buffer = Buffer.from('e298a218000000c042f15c9654050010abef8b5398f0d41212121212121212', 'hex')
       const frame = decodeFrame(buffer)
-      expect(frame.timestamp).toEqual(1500379664696000)
-      expect(frame.hash.equals(hashType('message.input.sensors'))).toBeTruthy()
-      expect(frame.payload.equals(new Buffer(8).fill(0x12))).toBeTruthy()
+      expect(frame).toEqual({
+        timestamp: 1500379664696000,
+        hash: hashType('message.input.sensors'),
+        payload: new Buffer(8).fill(0x12)
+      })
+    })
+  })
+
+  describe('roundtrip', () => {
+    it('decode than encode should equal original', () => {
+      const buffer = Buffer.from('e298a218000000c042f15c9654050010abef8b5398f0d41212121212121212', 'hex')
+      expect(encodeFrame(decodeFrame(buffer))).toEqual(buffer)
+    })
+
+    it('encode than decode should equal original', () => {
+      const frame = {
+        hash: hashType('message.input.sensors'),
+        timestamp: 1500379664696000,
+        payload: new Buffer(8).fill(0x12),
+      }
+      expect(decodeFrame(encodeFrame(frame))).toEqual(frame)
     })
   })
 })

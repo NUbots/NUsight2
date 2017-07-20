cfc090967e423d2b741b393643ffe0355bdac591
Brendan Annable <brendan.a@canva.com>
2017-07-20 21:40:26 +1000

    .

--- src/server/nbs/tests/nbs_frame_chunker.tests.ts
+++ src/server/nbs/tests/nbs_frame_chunker.tests.ts
@@ -1,16 +1,13 @@
 import { NbsFrameChunker } from '../nbs_frame_chunker'
-import { FakeNbsStream } from './fake_nbs_binary_stream'
 import { encodeFrame } from '../nbs_frame_codecs'
 import { hashType } from '../../nuclearnet/fake_nuclearnet_server'
 import { SeededRandom } from '../../../shared/base/random/seeded_random'
 
 describe('NbsFrameChunker', () => {
-  let stream: FakeNbsStream
   let chunker: NbsFrameChunker
   let random: SeededRandom
 
   beforeEach(() => {
-    stream = FakeNbsStream.of()
     chunker = NbsFrameChunker.of()
     random = SeededRandom.of('NbsFrameChunker')
   })
@@ -33,17 +30,17 @@ describe('NbsFrameChunker', () => {
     const buffer2 = fakeFrameBuffer(1)
     const buffer3 = fakeFrameBuffer(3)
 
-    stream.pipe(chunker).on('data', spy).on('finish', () => {
+    chunker.on('data', spy).on('finish', () => {
       expect(spy).toHaveBeenCalledWith(buffer1)
       expect(spy).toHaveBeenCalledWith(buffer2)
       expect(spy).toHaveBeenCalledWith(buffer3)
       done()
     })
 
-    stream.write(buffer1)
-    stream.write(buffer2)
-    stream.write(buffer3)
-    stream.end()
+    chunker.write(buffer1)
+    chunker.write(buffer2)
+    chunker.write(buffer3)
+    chunker.end()
   })
 
   it('synchronises and finds the expected frames with random leading bytes', done => {
@@ -52,18 +49,18 @@ describe('NbsFrameChunker', () => {
     const buffer2 = fakeFrameBuffer(1)
     const buffer3 = fakeFrameBuffer(2)
 
-    stream.pipe(chunker).on('data', spy).on('finish', () => {
+    chunker.on('data', spy).on('finish', () => {
       expect(spy).toHaveBeenCalledWith(buffer1)
       expect(spy).toHaveBeenCalledWith(buffer2)
       expect(spy).toHaveBeenCalledWith(buffer3)
       done()
     })
 
-    stream.write(new Buffer(32).fill(randomByte()))
-    stream.write(buffer1)
-    stream.write(buffer2)
-    stream.write(buffer3)
-    stream.end()
+    chunker.write(new Buffer(32).fill(randomByte()))
+    chunker.write(buffer1)
+    chunker.write(buffer2)
+    chunker.write(buffer3)
+    chunker.end()
   })
 
   it('synchronises and finds the expected frames with random interleaving bytes', done => {
@@ -72,20 +69,20 @@ describe('NbsFrameChunker', () => {
     const buffer2 = fakeFrameBuffer(1)
     const buffer3 = fakeFrameBuffer(2)
 
-    stream.pipe(chunker).on('data', spy).on('finish', () => {
+    chunker.on('data', spy).on('finish', () => {
       expect(spy).toHaveBeenCalledWith(buffer1)
       expect(spy).toHaveBeenCalledWith(buffer2)
       expect(spy).toHaveBeenCalledWith(buffer3)
       done()
     })
 
-    stream.write(new Buffer(32).fill(randomByte()))
-    stream.write(buffer1)
-    stream.write(new Buffer(32).fill(randomByte()))
-    stream.write(buffer2)
-    stream.write(new Buffer(32).fill(randomByte()))
-    stream.write(buffer3)
-    stream.write(new Buffer(32).fill(randomByte()))
-    stream.end()
+    chunker.write(new Buffer(32).fill(randomByte()))
+    chunker.write(buffer1)
+    chunker.write(new Buffer(32).fill(randomByte()))
+    chunker.write(buffer2)
+    chunker.write(new Buffer(32).fill(randomByte()))
+    chunker.write(buffer3)
+    chunker.write(new Buffer(32).fill(randomByte()))
+    chunker.end()
   })
 })

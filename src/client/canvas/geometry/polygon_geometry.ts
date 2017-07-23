27142ca679d3381dbf7f946f8ab9fa52901c3595
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2017-07-23 19:49:25 +1000

    Move polygon exception to constructor

--- src/client/canvas/geometry/polygon_geometry.ts
+++ src/client/canvas/geometry/polygon_geometry.ts
@@ -5,13 +5,14 @@ export class PolygonGeometry {
   @observable public points: Vector2[]
 
   constructor(opts: PolygonGeometry) {
-    this.points = opts.points
-  }
-
-  public static of(points: Vector2[]): PolygonGeometry {
+    const points = opts.points
     if (points.length < 3) {
       throw new Error('Polygon must have 3 or more points.')
     }
+    this.points = points
+  }
+
+  public static of(points: Vector2[]): PolygonGeometry {
     return new PolygonGeometry({ points })
   }
 }

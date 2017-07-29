e930a79e04764237d23e94cc2165496295085426
Brendan Annable <brendan@annable.id.au>
2017-07-29 11:38:31 +1000

    PR changes

--- src/client/time/browser_clock.ts
+++ src/client/time/browser_clock.ts
@@ -1,27 +1,30 @@
 import { CancelTimer } from '../../shared/time/clock'
 import { Clock } from '../../shared/time/clock'
 
+const SecondsToMilliseconds = 1e3
+const MillisecondsToSeconds = 1e-3
+
 function setTimeout(cb: (...args: any[]) => void, seconds: number): CancelTimer {
-  const handle = window.setTimeout(cb, seconds * 1e3)
-  return window.clearTimeout.bind(null, handle)
+  const handle = window.setTimeout(cb, seconds * SecondsToMilliseconds)
+  return window.clearTimeout.bind(undefined, handle)
 }
 
 function setInterval(cb: (...args: any[]) => void, seconds: number): CancelTimer {
-  const handle = window.setInterval(cb, seconds * 1e3)
-  return window.clearInterval.bind(null, handle)
+  const handle = window.setInterval(cb, seconds * SecondsToMilliseconds)
+  return window.clearInterval.bind(undefined, handle)
 }
 
 function setImmediate(cb: (...args: any[]) => void): CancelTimer {
   const handle = window.setTimeout(cb, 0)
-  return window.clearTimeout.bind(null, handle)
+  return window.clearTimeout.bind(undefined, handle)
 }
 
 function performanceNow() {
-  return window.performance.now() * 1e-3
+  return window.performance.now() * MillisecondsToSeconds
 }
 
 export const BrowserSystemClock: Clock = {
-  now: () => Date.now() * 1e-3,
+  now: () => Date.now() * MillisecondsToSeconds,
   performanceNow,
   setTimeout,
   setInterval,

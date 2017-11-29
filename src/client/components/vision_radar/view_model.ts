25f20b7cf55f401ba30fff8d7a94f3dcfda79fea
Brendan Annable <brendan@annable.me>
2017-11-29 22:16:30 +1100

    Switch to BufferGeometry

--- src/client/components/vision_radar/view_model.ts
+++ src/client/components/vision_radar/view_model.ts
@@ -5,15 +5,14 @@ import { Renderer } from 'three'
 import { Scene } from 'three'
 import { OrthographicCamera } from 'three'
 import { Camera } from 'three'
+import { Mesh } from 'three'
+import { Material } from 'three'
+import { MeshBasicMaterial } from 'three'
+import { Vector3 } from 'three'
+import { BufferGeometry } from 'three'
+import { Float32BufferAttribute } from 'three'
 import { VisionRadarModel } from './model'
 import { VisionRadarRobotModel } from './model'
-import { Object3D } from 'three'
-import { Mesh } from 'three'
-import { ShaderMaterial } from 'three'
-import { Material } from 'three'
-import { Geometry } from 'three'
-import { CircleGeometry } from 'three'
-import { MeshBasicMaterial } from 'three'
 
 export class VisionRadarViewModel {
   constructor(private model: VisionRadarModel) {
@@ -33,7 +32,7 @@ export class VisionRadarRobotViewModel {
   public renderer = createTransformer((canvas: HTMLCanvasElement): Renderer => {
     return new WebGLRenderer({ canvas })
   })
-  
+
   constructor(private model: VisionRadarRobotModel) {
   }
 
@@ -61,8 +60,25 @@ export class VisionRadarRobotViewModel {
   }
 
   @computed
-  get radarGeometry(): Geometry {
-    return new CircleGeometry(1, 50)
+  get radarGeometry(): BufferGeometry {
+    const geometry = new BufferGeometry()
+    const indices = []
+    const vertices = []
+    const vertex = new Vector3()
+    vertices.push(0, 0, 0)
+    const numSegments = 50
+    for (let i = 0; i <= numSegments; i++) {
+      const theta = i * 2 * Math.PI / numSegments
+      vertex.x = Math.cos(theta)
+      vertex.y = Math.sin(theta)
+      vertices.push(vertex.x, vertex.y, vertex.z)
+    }
+    for (let i = 1; i <= numSegments; i++) {
+      indices.push(i, i + 1, 0)
+    }
+    geometry.setIndex(indices)
+    geometry.addAttribute('position', new Float32BufferAttribute(vertices, 3))
+    return geometry
   }
 
   @computed

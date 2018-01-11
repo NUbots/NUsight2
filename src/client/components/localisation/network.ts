3500d8d9080f770f86492e5dea9fa628fce6ff2e
Trent Houliston <trent@houliston.me>
2018-01-12 01:32:53 +1100

    Update the protocol buffers and add scripts to regenerate in the future

--- src/client/components/localisation/network.ts
+++ src/client/components/localisation/network.ts
@@ -3,7 +3,7 @@ import { Matrix4 } from 'three'
 import { Quaternion } from 'three'
 import { Vector3 } from 'three'
 import { message } from '../../../shared/proto/messages'
-import { mat44$Properties } from '../../../shared/proto/messages'
+import { mat4$Properties } from '../../../shared/proto/messages'
 import { Network } from '../../network/network'
 import { NUsightNetwork } from '../../network/nusight_network'
 import { RobotModel } from '../robot/model'
@@ -30,7 +30,7 @@ export class LocalisationNetwork {
   private onSensors = (robotModel: RobotModel, sensors: Sensors) => {
     const robot = LocalisationRobotModel.of(robotModel)
 
-    const { translation: rWTt, rotation: Rwt } = decompose(new Matrix4().getInverse(fromProtoMat44(sensors.world!)))
+    const { translation: rWTt, rotation: Rwt } = decompose(new Matrix4().getInverse(fromProtoMat4(sensors.world!)))
     robot.rWTt.set(rWTt.x, rWTt.y, rWTt.z)
     robot.Rwt.set(Rwt.x, Rwt.y, Rwt.z, Rwt.w)
 
@@ -65,7 +65,7 @@ function decompose(m: Matrix4): { translation: Vector3, rotation: Quaternion, sc
   return { translation, rotation, scale }
 }
 
-function fromProtoMat44(m: mat44$Properties): Matrix4 {
+function fromProtoMat4(m: mat4$Properties): Matrix4 {
   return new Matrix4().set(
     m!.x!.x!, m!.y!.x!, m!.z!.x!, m!.t!.x!,
     m!.x!.y!, m!.y!.y!, m!.z!.y!, m!.t!.y!,

139c814a1538a9952e79ffd6c643f5ce7233041c
Brendan Annable <brendan@annable.me>
2018-01-14 00:47:08 +1100

    Add unbounded buffer and drain

--- src/server/nbs/nuclearnet_stream.ts
+++ src/server/nbs/nuclearnet_stream.ts
@@ -9,42 +9,69 @@ export type StreamJoinEvent = { type: 'nuclear_join', peer: NUClearNetPeer }
 export type StreamLeaveEvent = { type: 'nuclear_leave', peer: NUClearNetPeer }
 
 export class NUClearNetStream extends stream.Readable {
-  constructor(private nuclearnetClient: NUClearNetClient) {
+  private buffer: StreamPacket[] = []
+  private buffering = false
+
+  constructor(private nuclearnetClient: NUClearNetClient, private highWaterMark: number = 16) {
     super({
       objectMode: true,
+      highWaterMark,
     })
     nuclearnetClient.onPacket(this.onPacket)
     nuclearnetClient.onJoin(this.onJoin)
     nuclearnetClient.onLeave(this.onLeave)
   }
 
-  public static of(nuclearnetClient: NUClearNetClient) {
-    return new NUClearNetStream(nuclearnetClient)
+  public static of(nuclearnetClient: NUClearNetClient, highWaterMark: number) {
+    return new NUClearNetStream(nuclearnetClient, highWaterMark)
   }
 
-  push(event: StreamEvent | null): boolean {
+  public push(event: StreamEvent | null): boolean {
     return super.push(event)
   }
 
-  onPacket = (packet: NUClearNetPacket) => {
-    this.push({ type: 'packet', packet })
+  private bufferEvent(event: StreamPacket) {
+    this.buffer.push(event)
   }
 
-  onJoin = (peer: NUClearNetPeer) => {
+  private onDrain = () => {
+    this.buffering = false
+    while (this.buffer.length > 0) {
+      const event = this.buffer.shift()!
+      if (!this.onPacket(event.packet)) {
+        break
+      }
+    }
+  }
+
+  private onPacket = (packet: NUClearNetPacket): boolean => {
+    const event: StreamPacket = { type: 'packet', packet }
+    if (this.buffering) {
+      this.bufferEvent(event)
+      return false
+    } else if (!this.push(event)) {
+      this.buffering = true
+      return false
+    }
+    return true
+  }
+
+  private onJoin = (peer: NUClearNetPeer) => {
     this.push({ type: 'nuclear_join', peer })
   }
 
-  onLeave = (peer: NUClearNetPeer) => {
+  private onLeave = (peer: NUClearNetPeer) => {
     this.push({ type: 'nuclear_leave', peer })
   }
 
-  _read() {
-    // TODO (Annable): Is an empty implementation fine?
-    // Intentionally empty
+  public _read(size: number) {
+    if (this.buffering) {
+      this.onDrain()
+    }
   }
 
   // TODO (Annable): Automatically end when client disconnects?
-  end() {
+  public end() {
     this.push(null)
   }
 }
@@ -65,7 +92,7 @@ export class PeerFilter extends stream.Transform {
   }
 
   write(event: StreamEvent) {
-    return super.write(event);
+    return super.write(event)
   }
 
   _transform(event: StreamEvent, encoding: string, done: (err?: any, data?: any) => void) {
--- src/server/nbs/tests/nuclearnet_stream.tests.ts
+++ src/server/nbs/tests/nuclearnet_stream.tests.ts
@@ -1,4 +1,7 @@
+import { NUClearNetPacket } from 'nuclearnet.js'
 import { NUClearNetPeer } from 'nuclearnet.js'
+import { PassThrough } from 'stream'
+import { range } from '../../../shared/base/range'
 import { FakeNUClearNetClient } from '../../nuclearnet/fake_nuclearnet_client'
 import { FakeNUClearNetServer } from '../../nuclearnet/fake_nuclearnet_server'
 import { StreamPacket } from '../nuclearnet_stream'
@@ -6,6 +9,7 @@ import { PeerFilter } from '../nuclearnet_stream'
 import { NUClearNetStream } from '../nuclearnet_stream'
 
 describe('NUClearNetStream', () => {
+  const highWaterMark = 10
   let nuclearnetServer: FakeNUClearNetServer
   let nuclearnetClient: FakeNUClearNetClient
   let stream: NUClearNetStream
@@ -13,7 +17,7 @@ describe('NUClearNetStream', () => {
   beforeEach(() => {
     nuclearnetServer = new FakeNUClearNetServer()
     nuclearnetClient = new FakeNUClearNetClient(nuclearnetServer)
-    stream = NUClearNetStream.of(nuclearnetClient)
+    stream = NUClearNetStream.of(nuclearnetClient, highWaterMark)
   })
 
   it('forwards join events into the stream', done => {
@@ -69,7 +73,58 @@ describe('NUClearNetStream', () => {
     nuclearnetClient.fakePacket('hash', packet)
     stream.end()
   })
+
+  describe('buffering', () => {
+    let packet: NUClearNetPacket
+
+    beforeEach(() => {
+      packet = {
+        peer: { name: 'alice', address: 'fake_address', port: -1 },
+        hash: new Buffer(8),
+        payload: new Buffer(8),
+        reliable: true,
+      }
+      nuclearnetClient.connect({ name: 'bob' })
+    })
+
+    it('stops at highwatermark', done => {
+      const spy = jest.fn()
+      stream.on('data', spy).on('end', () => {
+        expect(spy).toHaveBeenCalledTimes(highWaterMark)
+        done()
+      })
+      range(highWaterMark + 10).forEach(() => {
+        nuclearnetClient.fakePacket('hash', packet)
+      })
+      stream.end()
+    })
+
+    it('drains buffer when available', async done => {
+      const slowReader = new PassThrough({ objectMode: true })
+      const spy = jest.fn()
+
+      slowReader.on('data', spy).on('end', () => {
+        const packetEventCalls = spy.mock.calls.filter(call => call[0].type === 'packet')
+        expect(packetEventCalls.length).toBe(100)
+        packetEventCalls.forEach(call => {
+          expect(call[0]).toMatchObject({ type: 'packet', packet })
+        })
+        done()
+      })
+      stream.pipe(slowReader)
+
+      range(highWaterMark + 90).forEach(() => {
+        nuclearnetClient.fakePacket('hash', packet)
+      })
+
+      await flush()
+
+      stream.end()
+    })
+  })
 })
+
+const flush = () => new Promise(res => setImmediate(res))
 
 describe('PeerFilter', () => {
   let peer: NUClearNetPeer

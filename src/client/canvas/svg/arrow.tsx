ac0888a4f0c7afdc176bdf044425aac2983388f2
Trent Houliston <trent@houliston.me>
2018-01-14 13:46:35 +1100

    Add drawing arrows

--- src/client/canvas/svg/arrow.tsx
+++ src/client/canvas/svg/arrow.tsx
@@ -11,7 +11,24 @@ import { appearance } from './svg'
 export class Arrow extends Component<{model: Shape<ArrowGeometry>}> {
   // TODO draw arrows using lines
   public render() {
-    const g = this.props.model.geometry
-    return <g/>
+    const m = this.props.model
+    const g = m.geometry
+    const w = g.width * 0.5
+    const hl = g.headLength * 0.5
+    const hw = g.headWidth * 0.5
+    const r = (180 / Math.PI) * Math.atan2(g.direction.y, g.direction.x)
+
+    let path = `M0 ${-w}`
+    path += `L${g.length - hl} ${-w}`
+    path += `L${g.length - hl} ${-hw}`
+    path += `L${g.length} 0`
+    path += `L${g.length - hl} ${hw}`
+    path += `L${g.length - hl} ${w}`
+    path += `L0 ${w}`
+
+    return <path
+      d={path}
+      transform={`translate(${g.origin.x},${g.origin.y}) rotate(${r})`}
+      {...appearance(m.appearance)}/>
   }
 }

5c8241e6a770066fb359a58c205c4aaa0c953adb
Trent Houliston <trent@houliston.me>
2018-01-14 13:52:20 +1100

    Fix rendering circles

--- src/client/canvas/svg/circle.tsx
+++ src/client/canvas/svg/circle.tsx
@@ -13,6 +13,6 @@ export class Circle extends Component<{model: Shape<CircleGeometry>}> {
   public render() {
     const m = this.props.model
     const g = m.geometry
-    return <circle cx={g.x} cy={g.y} radius={g.radius} {...appearance(m.appearance)} />
+    return <circle cx={g.x} cy={g.y} r={g.radius} {...appearance(m.appearance)} />
   }
 }

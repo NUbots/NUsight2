9214b80f3b8763e8ebab610e8dcedb4a4ad901db
Trent Houliston <trent@houliston.me>
2018-01-17 15:21:48 +1100

    Missed scoping

--- src/client/render2d/canvas_renderer.tsx
+++ src/client/render2d/canvas_renderer.tsx
@@ -38,7 +38,7 @@ export class CanvasRenderer extends Component<RendererProps> {
 
   render() {
     return (
-      <div className={classNames(className, style.container)}>
+      <div className={classNames(this.props.className, style.container)}>
         <ReactResizeDetector handleWidth handleHeight onResize={this.onResize} />
         <canvas
           className={style.container}

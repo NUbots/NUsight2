0dd1305f5e5a1d138fbc4949d121491bf72b7af5
Trent Houliston <trent@houliston.me>
2018-04-10 19:19:43 +1000

    Fix tests

--- src/client/render2d/svg/tests/path.tests.tsx
+++ src/client/render2d/svg/tests/path.tests.tsx
@@ -4,7 +4,7 @@ import { Transform } from '../../../math/transform'
 import { BasicAppearance } from '../../appearance/basic_appearance'
 import { PathGeometry } from '../../geometry/path_geometry'
 import { Shape } from '../../object/shape'
-import { Path } from '../Path'
+import { Path } from '../path'
 
 describe('PathSVGRenderer', () => {
   it('renders', () => {

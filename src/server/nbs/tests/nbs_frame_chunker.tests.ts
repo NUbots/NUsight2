7925005c82b8ee22d842e540a666bc6616a909ee
Trent Houliston <trent@houliston.me>
2018-04-27 21:17:17 +1000

    Add unit test to ensure the chunker handles partial packets properly

--- src/server/nbs/tests/nbs_frame_chunker.tests.ts
+++ src/server/nbs/tests/nbs_frame_chunker.tests.ts
@@ -12,11 +12,11 @@ describe('NbsFrameChunker', () => {
     random = SeededRandom.of('NbsFrameChunker')
   })
 
-  function fakeFrameBuffer(timestampInMicroseconds: number) {
+  function fakeFrameBuffer(timestampInMicroseconds: number, size: number = 8) {
     return encodeFrame({
       timestampInMicroseconds,
       hash: hashType('fake'),
-      payload: new Buffer(8).fill(randomByte()),
+      payload: new Buffer(size).fill(randomByte()),
     })
   }
 
@@ -83,6 +83,33 @@ describe('NbsFrameChunker', () => {
     chunker.write(new Buffer(32).fill(randomByte()))
     chunker.write(buffer3)
     chunker.write(new Buffer(32).fill(randomByte()))
+    chunker.end()
+  })
+
+  it('handles packets split over multiple buffers', done => {
+    const spy = jest.fn()
+
+    const buffer1 = fakeFrameBuffer(0, 200)
+    const buffer2 = fakeFrameBuffer(0, 200)
+    const buffer3 = fakeFrameBuffer(0, 200)
+
+    chunker.on('data', spy).on('finish', () => {
+      expect(spy).toHaveBeenCalledWith(buffer1)
+      expect(spy).toHaveBeenCalledWith(buffer2)
+      expect(spy).toHaveBeenCalledWith(buffer3)
+      done()
+    })
+
+    // Randomly split up the buffer into bits
+    let fullBuffer = Buffer.concat([buffer1, buffer2, buffer3])
+
+    while (fullBuffer.length > 0) {
+      // Split 100 or less to ensure we get small chunks
+      const split = random.integer(1, Math.min(100, fullBuffer.length))
+      chunker.write(fullBuffer.slice(0, split))
+      fullBuffer = fullBuffer.slice(split)
+    }
+
     chunker.end()
   })
 })

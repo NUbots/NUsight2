529ee760652f2f5164dc871dd47f57614d7649c5
Brendan Annable <brendan@annable.me>
2018-08-13 00:57:04 +1000

    Add Webpack HMR

--- src/client/components/app/view.tsx
+++ src/client/components/app/view.tsx
@@ -1,22 +1,48 @@
+import { action } from 'mobx'
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
+import { Component } from 'react'
 import * as React from 'react'
+import { hot } from 'react-hot-loader'
+import { Switch } from 'react-router'
+import { Route } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 
+import { install } from '../../install'
 import { NavigationConfiguration } from '../../navigation'
 import { NavigationView } from '../navigation/view'
 
 import * as style from './style.css'
 
-export class AppView extends React.Component<{ nav: NavigationConfiguration }> {
+@observer
+class AppView extends Component {
+  @observable.ref
+  private nav?: NavigationConfiguration
+
+  @action
+  componentDidMount() {
+    this.nav = install()
+  }
+
   render() {
-    const { nav, children } = this.props
-    return (
-      <div className={style.app}>
-        <NavigationView nav={nav}/>
-        <div className={style.app__container}>
-          <div className={style.app__content}>
-            {children}
+    return this.nav ? (
+      <BrowserRouter>
+        <div className={style.app}>
+          <NavigationView nav={this.nav}/>
+          <div className={style.app__container}>
+            <div className={style.app__content}>
+              <Switch>
+                {...this.nav.getRoutes().map(config => (
+                  <Route key={config.path} exact={config.exact} path={config.path} render={() => <config.Content/>}/>
+                ))}
+              </Switch>
+            </div>
           </div>
         </div>
-      </div>
-    )
+      </BrowserRouter>
+    ) : null
   }
 }
+
+
+export default hot(module)(AppView)
---
+++ src/client/install.ts
@@ -0,0 +1,30 @@
+import { AppController } from './components/app/controller'
+import { AppModel } from './components/app/model'
+import { AppNetwork } from './components/app/network'
+import { installChart } from './components/chart/install'
+import { installDashboard } from './components/dashboard/install'
+import { installLocalisation } from './components/localisation/install'
+import { withRobotSelectorMenuBar } from './components/menu_bar/view'
+import { installVision } from './components/vision/install'
+import { installVisualMesh } from './components/visual_mesh/install'
+import { NavigationConfiguration } from './navigation'
+import { NUsightNetwork } from './network/nusight_network'
+
+export function install() {
+  const nav = NavigationConfiguration.of()
+  const appModel = AppModel.of()
+  const nusightNetwork = NUsightNetwork.of(appModel)
+  nusightNetwork.connect({ name: 'nusight', address: '10.1.255.255' })
+
+  const appController = AppController.of()
+  AppNetwork.of(nusightNetwork, appModel)
+  const menu = withRobotSelectorMenuBar(appModel.robots, appController.toggleRobotEnabled)
+
+  installDashboard({ nav, appModel, nusightNetwork, menu })
+  installLocalisation({ nav, appModel, nusightNetwork, menu })
+  installChart({ nav, appModel, nusightNetwork, menu })
+  installVision({ nav, appModel, nusightNetwork, Menu: menu })
+  installVisualMesh({ nav, appModel, nusightNetwork, Menu: menu })
+
+  return nav
+}

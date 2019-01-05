cc01171d98ec8d6a34777c51bb9d69d4353170c2
Brendan Annable <brendan@annable.me>
2019-01-05 17:48:14 +1100

    Add priority queue to improve network performance

---
+++ src/server/nuclearnet/lru_priority_queue.ts
@@ -0,0 +1,40 @@
+type Item<K, V> = {
+  type: K,
+  values: V[]
+}
+
+export class LruPriorityQueue<K, V> {
+  private readonly limitPerKey?: number
+
+  queue: Array<Item<K, V>> = []
+  map: Map<K, Item<K, V>> = new Map()
+
+  constructor({ limitPerKey }: { limitPerKey?: number } = {}) {
+    this.limitPerKey = limitPerKey
+  }
+
+  add(k: K, v: V) {
+    let item = this.map.get(k)
+    if (!item) {
+      item = { type: k, values: [] }
+      this.map.set(k, item)
+      this.queue.push(item)
+    }
+    if (this.limitPerKey && item.values.length >= this.limitPerKey) {
+      item.values.shift()
+    }
+    item.values.push(v)
+  }
+
+  next(): { key: K, value: V } | undefined {
+    /* tslint:disable-next-line prefer-for-of */
+    for (let i = 0; i < this.queue.length; i++) {
+      const item = this.queue.shift()!
+      this.queue.push(item)
+      const nextValue = item.values.shift()
+      if (nextValue) {
+        return { key: item.type, value: nextValue }
+      }
+    }
+  }
+}
---
+++ src/server/nuclearnet/tests/lru_priority_queue.tests.ts
@@ -0,0 +1,49 @@
+import { LruPriorityQueue } from '../lru_priority_queue'
+
+describe('LruPriorityQueue', () => {
+  /** Fetch all remaining items in the queue, in priority order.  */
+  function all<K, V>(queue: LruPriorityQueue<K, V>): Array<{ key: K, value: V }> {
+    const items: Array<{ key: K, value: V }> = []
+    let item: { key: K, value: V } | undefined
+    /* tslint:disable-next-line no-conditional-assignment */
+    while ((item = queue.next())) {
+      items.push(item)
+    }
+    return items
+  }
+
+  it('interleaves keys', () => {
+    const queue = new LruPriorityQueue<string, string>()
+    queue.add('foo', 'foo_1')
+    queue.add('foo', 'foo_2')
+    queue.add('bar', 'bar_1')
+    queue.add('bar', 'bar_2')
+    queue.add('baz', 'baz_1')
+    queue.add('baz', 'baz_2')
+    expect(all(queue)).toEqual([
+      { key: 'foo', value: 'foo_1' },
+      { key: 'bar', value: 'bar_1' },
+      { key: 'baz', value: 'baz_1' },
+      { key: 'foo', value: 'foo_2' },
+      { key: 'bar', value: 'bar_2' },
+      { key: 'baz', value: 'baz_2' },
+    ])
+  })
+
+  it('drops items when over limit', () => {
+    const queue = new LruPriorityQueue<string, string>({ limitPerKey: 2 })
+    queue.add('foo', 'foo_1')
+    queue.add('foo', 'foo_2')
+    queue.add('foo', 'foo_3')
+    queue.add('foo', 'foo_4')
+    queue.add('bar', 'bar_1')
+    queue.add('bar', 'bar_2')
+    queue.add('bar', 'bar_3')
+    expect(all(queue)).toEqual([
+      { key: 'foo', value: 'foo_3' },
+      { key: 'bar', value: 'bar_2' },
+      { key: 'foo', value: 'foo_4' },
+      { key: 'bar', value: 'bar_3' },
+    ])
+  })
+})

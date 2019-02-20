12cb95705a4bbe04ac1c1a1691eb13d9ccbb3347
Josephus Paye II <j.paye96@gmail.com>
2019-02-20 19:27:57 +1100

    Comment use of inline <style>

--- src/client/components/storybook/fullscreen.tsx
+++ src/client/components/storybook/fullscreen.tsx
@@ -5,6 +5,10 @@ import * as style from './fullscreen.css'
 
 export function fullscreen(story: RenderFunction) {
   return <div className={style.fullscreen}>
+    {
+      // This is an inline <style> and not in `fullscreen.css` as that would make it apply globally
+      // to all stories, due to the way we build and extract the styles from CSS files.
+    }
     <style>{`body { margin: 0; padding: 0; }`}</style>
     {story()}
   </div>

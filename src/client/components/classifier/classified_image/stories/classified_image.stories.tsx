fe819156cbc8160c99f24ba01b9f7f07de219ba8
Brendan Annable <brendan@annable.me>
2019-03-02 21:16:33 +1100

    add animated story

--- src/client/components/classifier/classified_image/stories/classified_image.stories.tsx
+++ src/client/components/classifier/classified_image/stories/classified_image.stories.tsx
@@ -1,10 +1,15 @@
 import { storiesOf } from '@storybook/react'
+import { action } from 'mobx'
+import { reaction } from 'mobx'
 import { runInAction } from 'mobx'
+import { disposeOnUnmount } from 'mobx-react'
 import { observer } from 'mobx-react'
+import { now } from 'mobx-utils'
 import { Component } from 'react'
 import * as React from 'react'
 
 import { SeededRandom } from '../../../../../shared/base/random/seeded_random'
+import { range } from '../../../../../shared/base/range'
 import { fullscreen } from '../../../storybook/fullscreen'
 import { Classification } from '../../classifications'
 import { Lut } from '../../lut'
@@ -19,18 +24,41 @@ storiesOf('classifier.classified_image', module)
     const random = SeededRandom.of('classifier')
     const lut = generateLut(random)
     const model = ClassifiedImageModel.of({ lut })
-    return <ClassifiedImageViewHarness model={model}/>
+    return <ClassifiedImageViewHarness model={model} random={random}/>
+  })
+  .add('renders animated', () => {
+    const random = SeededRandom.of('classifier')
+    const lut = generateLut(random)
+    const model = ClassifiedImageModel.of({ lut })
+    return <ClassifiedImageViewHarness model={model} random={random} animate/>
   })
 
 @observer
-class ClassifiedImageViewHarness extends Component<{ model: ClassifiedImageModel }> {
+class ClassifiedImageViewHarness extends Component<{
+  model: ClassifiedImageModel,
+  random: SeededRandom,
+  animate?: boolean
+}> {
   async componentDidMount() {
     const image = await loadImage(imageUrl)
     runInAction(() => this.props.model.image = { type: 'image', image })
+    disposeOnUnmount(this, reaction(() => this.props.animate && now('frame'), this.update))
   }
 
   render() {
     return <ClassifiedImageView model={this.props.model}/>
+  }
+
+  @action.bound
+  private update() {
+    const percentageFull = 0.4
+    range(100).forEach(() => {
+      const { random, model: { lut } } = this.props
+      const randomIndex = random.integer(0, lut.data.length)
+      const randomClassification = random.float() <= percentageFull
+        ? random.choice(classifications) : Classification.Unclassified
+      lut.set(randomIndex, randomClassification)
+    })
   }
 }
 

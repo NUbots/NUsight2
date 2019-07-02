6108d629896790f9189b8be8ec8b0d834b4fe2fe
Josephus Paye II <j.paye96@gmail.com>
2019-07-02 18:18:05 +1000

    Revert `Clock` changes

--- src/shared/time/clock.ts
+++ src/shared/time/clock.ts
@@ -1,6 +1,3 @@
-import { BrowserSystemClock } from '../../client/time/browser_clock'
-import { NodeSystemClock } from '../../server/time/node_clock'
-
 export interface Clock {
   now(): number
   date(): Date
@@ -11,12 +8,3 @@ export interface Clock {
 }
 
 export type CancelTimer = () => void
-
-const isNode =
-  typeof process === 'object' &&
-    typeof process.versions === 'object' &&
-    typeof process.versions.node === 'string'
-
-export function getClock(): Clock {
-  return isNode ? NodeSystemClock : BrowserSystemClock
-}

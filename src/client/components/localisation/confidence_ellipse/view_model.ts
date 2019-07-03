d8f1b93b3466950cebbab2337482f47697acd6c1
Brendan Annable <brendan@annable.me>
2019-07-03 14:26:52 +1000

    .

--- src/client/components/localisation/confidence_ellipse/view_model.ts
+++ src/client/components/localisation/confidence_ellipse/view_model.ts
@@ -36,7 +36,8 @@ export class ConfidenceEllipseViewModel {
 
   @computed
   get ellipsePosition() {
-    const rWTw = toThreeVector3(this.robot.rTWw).applyQuaternion(toThreeQuaternion(this.robot.Rwt))
+    // debugger
+    const rWTw = toThreeVector3(this.robot.rTWw);//.applyQuaternion(toThreeQuaternion(this.robot.Rwt))
     // rWTw.multiplyScalar(-1)
     const rTFf = rWTw.applyMatrix4(toThreeMatrix4(this.robot.Hfw))
     return new Vector3(rTFf.x, rTFf.y, 0)

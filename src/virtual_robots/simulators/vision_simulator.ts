0fd45740e2c0eb724cb6d747470789470747a93c
Ysobel <ysobelsims@gmail.com>
2020-01-13 15:09:17 +1100

    Fixes messages for camera

--- src/virtual_robots/simulators/vision_simulator.ts
+++ src/virtual_robots/simulators/vision_simulator.ts
@@ -61,7 +61,7 @@ export class VisionSimulator extends Simulator {
         lens: {
           projection: Projection.RECTILINEAR,
           focalLength: 712,
-          fov: { x: 1, y: 1 },
+          fov: 1,
         },
       }).finish(),
     }
